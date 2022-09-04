1e40a0a32ebd7f18da887278573d250d605db049
Shaun Tornilla <70738856+ShaunTornilla@users.noreply.github.com>
2022-09-03 19:09:11 -0500

    Added Prototype1Runthrough

---
+++ Prototype1Runthrough/Assets/Scripts/LoseOnFall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Attach Script to Player
+
+public class LoseOnFall : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -1)
+        {
+            ScoreManager.gameOver = true;
+        }
+    }
+}
---
+++ Prototype1Runthrough/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float turnSpeed;
+
+    public float forwardInput;
+    public float horizontalInput;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Move Forward with Forward Input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}

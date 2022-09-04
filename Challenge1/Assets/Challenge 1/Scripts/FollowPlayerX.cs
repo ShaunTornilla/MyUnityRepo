22d297feebdec8bf26f5fd6d215334ade2ca8357
Shaun Tornilla <70738856+ShaunTornilla@users.noreply.github.com>
2022-09-03 23:39:15 -0500

    Challenge1

---
+++ Challenge1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Shaun Tornilla
+ * Challenge1
+ * Camera Functions
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(30, 0, 10);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Challenge1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Shaun Tornilla
+ * Challenge1
+ * Propeller Animations
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed);
+
+    }
+}

881d5e573686ff0577633169ba738d7f570c1997
Shaun Tornilla <70738856+ShaunTornilla@users.noreply.github.com>
2022-10-22 15:32:17 -0500

    Asssignment 5b Prototype Done

--- Assignment 5B/Assets/MyFirstPersonPlayer/Scripts/PlayerMovement.cs
+++ Assignment 5B/Assets/MyFirstPersonPlayer/Scripts/PlayerMovement.cs
@@ -7,12 +7,52 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
     public float speed = 12f;
 
+    // Variables for gravity
+    public Vector3 velocity;
+    public float gravity = -9.81f;
+    public float gravityMultiplier = 2f;
+
+    // Jump Variable
+    public float jumpHeight = 3f;
+
+    // GroundCheck variables
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public bool isGrounded;
+
+    public void Awake()
+    {
+        gravity *= gravityMultiplier;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        // Check if player is on ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        if(isGrounded && velocity.y < 0)
+        {
+            // Resets velocity back to zero to simulate gravity
+            velocity.y = -2; 
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Add Jump Code before gravity velocity
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Add gravity to velocity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
     }
 }
---
+++ Assignment 5B/Assets/MyFirstPersonPlayer/Scripts/ShootWithRaycasts.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWithRaycasts : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera cam;
+
+    public ParticleSystem muzzleFlash;
+
+    public float hitForce = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hitInfo;
+
+        // If our raycast (bullet) hits something
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
+        {
+            Debug.Log(hitInfo.transform.gameObject.name);
+
+
+
+            // Get the target script off hit object
+            Target target = hitInfo.transform.gameObject.GetComponent<Target>();
+
+            // If target script found, deal damage to target
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if(hitInfo.rigidbody != null)
+            {
+                hitInfo.rigidbody.AddForce(cam.transform.TransformDirection(Vector3.forward) * hitForce, ForceMode.Impulse);
+            }
+        }
+    }
+}

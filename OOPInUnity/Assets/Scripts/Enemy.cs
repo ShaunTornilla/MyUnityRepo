79b75b203bd423e263c8958c7072406f780b7a16
Shaun Tornilla <70738856+ShaunTornilla@users.noreply.github.com>
2022-11-02 01:26:42 -0500

    OOP Assignment (58:18)

---
+++ OOPInUnity/Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour, IDamageable
+{
+    protected float speed;
+    protected int health;
+
+    [SerializeField] protected Weapon weapon;
+
+
+    protected virtual void Awake()
+    {
+        weapon = gameObject.AddComponent<Weapon>();
+
+        weapon.damageBonus = 10; 
+
+        speed = 5f;
+        health = 100;
+    }
+
+    protected abstract void Attack(int amount);
+
+    public abstract void TakeDamage(int amount);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ OOPInUnity/Assets/Scripts/Golem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golem : Enemy
+{
+    protected int damage;
+
+
+
+    // Start is called before the first frame update
+    protected override void Awake()
+    {
+        // Gives us the ability to override the default health in Enemy.cs to what we want the Golem to be
+        base.Awake();
+
+        health = 120;
+    }
+
+    protected override void Attack(int amount)
+    {
+        Debug.Log("Golem Attacks!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void TakeDamage(int amount)
+    {
+        Debug.Log("You took " + amount + " points of damage!");
+    }
+}
